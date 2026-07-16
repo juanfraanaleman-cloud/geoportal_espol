8d3606c7d3aa41afafc84ef3085224ded36e56fd
juanfraanaleman-cloud <juanfraanaleman@gmail.com>
2026-07-16 16:13:50 -0500

    update

--- main.js
+++ main.js
@@ -160,7 +160,7 @@ const polig_espolStyle = new ol.style.Style({
 
 
 const polig_espol = new ol.layer.Vector({
-  source: new ol.source.Vector({ url: './capas/lin_espol.geojson', format: new ol.format.GeoJSON() }),
+  source: new ol.source.Vector({ url: './capas/polig_espol.geojson', format: new ol.format.GeoJSON() }),
   title: 'ESPOL',
   visible: false,
   style: function(feature) {
