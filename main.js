fe0a4175ff32b69d9d763b90a705eb3e0476f8cb
juanfraanaleman-cloud <juanfraanaleman@gmail.com>
2026-07-20 13:49:25 -0500

    update

--- main.js
+++ main.js
@@ -917,7 +917,9 @@ map.on('singleclick', function (evt) {
     
   } else {
     // Esconder popup al hacer click en espacio vacío
+    if (!map.hasFeatureAtPixel(evt.pixel)) {
     overlay.setPosition(undefined);
+  }
   }
 });
 
